320c7afc3d50f954bc8672b64e9fbcce5df90941
FeiYehua <31472675+feiyehua@users.noreply.github.com>
2025-03-08 23:38:29 +0800

    Add js file and README file

---
+++ Make Trump the SUN in US.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Make Trump the SUN in US
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Bold every Trump on Whitehouse website
+// @author       FeiYehua
+// @match        https://www.whitehouse.gov/*
+// @grant        MIT
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    // Specify the bolded textes here
+    const keyword = ['President Donald J. Trump','President Trump','Donald J. Trump','Donald Trump','Melania Trump','Barron Trump','Mr Trump','Trump'];
+    const style = 'font-weight: bold; font-size: 1.1em;';
+
+    // traverse the whole website
+    function walkNodes(node) {
+        if (node.nodeType === Node.TEXT_NODE) {
+            const tempDiv = document.createElement('div');
+            tempDiv.innerHTML = node.textContent.replace(
+                new RegExp(`\\b${keyword.join('|')}\\b`, 'gi'),
+                //match the target words
+                match => `<span style="${style}">${match}</span>`
+            );
+            while (tempDiv.firstChild) {
+                node.parentNode.insertBefore(tempDiv.firstChild, node);
+            }
+            node.parentNode.removeChild(node);
+        } else {
+            for (let child of node.childNodes) {
+                walkNodes(child);
+            }
+        }
+    }
+
+    window.addEventListener('DOMContentLoaded', function() {
+        walkNodes(document.body);
+    });
+
+})();
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Make-Trump-the-SUN-in-US
+****Trump**** is going to become the **sun** in the United States.
+
+Let's help The White House maintainers to bring **President Trump** even closer to his dream.
